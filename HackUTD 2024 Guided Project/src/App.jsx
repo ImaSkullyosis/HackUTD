ee1bdac9b976736c44d77b1870c09055e5883af4
Adrian <adrian.01@list.ru>
2024-11-16 17:25:12 -0600

    Modified the app.jsx to display the main page

--- HackUTD 2024 Guided Project/src/App.jsx
+++ HackUTD 2024 Guided Project/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import Land from "./pages/landingpage.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return <Land></Land>;
 }
 
 export default App;
--- HackUTD 2024 Guided Project/src/pages/landingpage.jsx
+++ HackUTD 2024 Guided Project/src/pages/landingpage.jsx
@@ -1,1 +1,12 @@
 import "./landingpage.css";
+import { useState } from "react";
+
+function land() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default land;
