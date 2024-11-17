a82156329b764d3f6413a2779063412aa984d6f2
Adrian <adrian.01@list.ru>
2024-11-17 00:14:46 -0600

    advice now recieves an array of selected issues

--- HackUTD 2024 Guided Project/src/pages/advice.jsx
+++ HackUTD 2024 Guided Project/src/pages/advice.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import "./advice.css";
 function test() {
-  return <h1>Hellow</h1>;
+  const location = useLocation();
+  const selected = location.state;
+  console.log(selected);
+  return <></>;
 }
 
 export default test;
